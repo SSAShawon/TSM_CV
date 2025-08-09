0013409af7be19a577fd80c7fc6434726bf01d26
Shawon <samsul261103@gmail.com>
2025-08-10 00:54:31 +0600

    1sti

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ function toggleGallery(id) {
   gallery.classList.toggle("hidden");
 }
 
+// Popup Elements
+const showIdBtn = document.getElementById("showIdBtn");
+const idPopup = document.getElementById("idPopup");
+const closeBtn = document.querySelector(".closeBtn");
+
+// Show Popup
+showIdBtn.addEventListener("click", () => {
+  idPopup.style.display = "flex";
+});
+
+// Hide Popup on Close button click
+closeBtn.addEventListener("click", () => {
+  idPopup.style.display = "none";
+});
+
+// Hide Popup if clicked outside content
+window.addEventListener("click", (e) => {
+  if (e.target === idPopup) {
+    idPopup.style.display = "none";
+  }
+});
